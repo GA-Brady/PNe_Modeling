cbdb5da70a1eaaa058c7b96c3d69cc9fbfc7bc16
Garrett Brady <gab5654@psu.edu>
2026-06-22 19:06:52 -0400

    minimal working recombination cross section within a single RR block. w/out .en lookup implementation

--- O2Plus_EigenAnalysis_V0.1.0-wip.jl
+++ O2Plus_EigenAnalysis_V0.1.0-wip.jl
@@ -57,31 +57,6 @@ begin
 	end
 end
 
-# ╔═╡ 8100381f-fe1a-46fd-a039-cd4d77c29d55
-begin
-	O4P_rr_file = joinpath(fac_dir, "O04a.rr")
-	rr_header, rr_data = rfac.read_rr(O4P_rr_file)
-	print(rr_data)
-
-	pyrr_PARAM 	= rr_data[0]["parameters"][0]
-	pyrr_EGRID 	= rr_data[0]["EGRID"]
-	pyrr_PIGRD 	= rr_data[0]["PI crosssection"][0]
-	pyrr_RRGRD  = rr_data[0]["RR crosssection"][0]
-	pyrr_gf 	= rr_data[0]["gf"][0]
-	pyrr_ΔE   	= rr_data[0]["Delta E"]
-
-	rr_ΔE    = pyconvert(Vector{Float64}, pyrr_ΔE)[1]
-	
-	rr_PARAM = pyconvert(Array{Float64}, pyrr_PARAM)
-	rr_EGRID = pyconvert(Vector{Float64}, pyrr_EGRID) 
-	rr_PIGRD = pyconvert(Vector{Float64}, pyrr_PIGRD)
-	rr_RRGRD = pyconvert(Vector{Float64}, pyrr_RRGRD)
-	rr_gf 	 = pyconvert(Vector{Float64}, pyrr_gf)
-end
-
-# ╔═╡ d79c83b9-f3e0-497a-8ddc-1473f789ac99
-FACrrunpacker(O4P_rr_file)
-
 # ╔═╡ fbb8819e-da92-4cd7-a3f0-811a27aa3ae4
 begin
 	struct Prr
@@ -126,46 +101,6 @@ begin
 		return energy, params, bindex, b2j, findex, f2j, deltal
 	end
 end
-
-# ╔═╡ f1a53e3c-fd60-4c8c-8931-66a92e28dc36
-begin
-	nelectrons = 4
-	OyxgenRRfile = joinpath(fac_dir, "O0$(nelectrons)a.rr")
-	RRheader, RRdata = rfac.read_rr(O4P_rr_file)
-
-	nblocks = pyconvert(Int64, RRheader["NBlocks"])
-
-	for i in 0:(nblocks-1)
-		# Iterating across all the blocks
-		# RRdata is still a python object at this point ∴ 0-indexed
-		
-	end
-	
-	# extracting vectors from the blocks
-	energy, params, bindex, b2j, findex, f2j, deltal = rrblockreader(RRdata[1])
-	prrvec = rrparamblockunpacker(params)
-
-	# masks to avoid unnecessary computation
-	bmask = bindex .!= 0  # only to excited states
-	fmask = findex .== 46 # only from ground state of O03
-	
-	# total mask of case B recombination
-	tmask = bmask .&& fmask
-
-	# filtering our values to the ones we really care about
-	filtered_energy = energy[tmask]
-	filtered_prrvec = prrvec[tmask]
-	filtered_bindex = bindex[tmask]
-	filtered_b2j    = b2j[tmask]
-	filtered_findex = findex[tmask]
-	filtered_f2j    = f2j[tmask]
-	filtered_deltal = deltal[tmask]
-
-	num = length(filtered_energy)
-end
-
-# ╔═╡ 451690bc-74ba-461b-bb1d-f63011d8db08
-rrparamblockunpacker(params)
 
 # ╔═╡ 42ab7cc6-c06a-4d84-ba76-34c214526cbe
 begin
@@ -212,7 +147,7 @@ begin
 	"""
 	Photoionization and radiative crosssection functions
 	"""
-	function bfoscstrength(photonenergy::Float64, p::Vector{Float64}, lb, thermalizingenergy)
+	function bfoscstrength(photonenergy::Float64, p::Prr, lb, thermalizingenergy)
 		# l 	:= orbital angular momentum of ionized shell
 		
 		ΔE = photonenergy - thermalizingenergy
@@ -221,9 +156,9 @@ begin
 			return 0
 		end
 		
-		x = abs((ΔE + p[4]) / p[4])
-		y = (1 + p[3]) / (sqrt(x) + p[3])
-		dgf = photonenergy / (ΔE+ p[4]) * p[1] * (x ^ (-3.5 - lb + 0.5 * p[2])) * y ^ p[2]
+		x = abs((ΔE + p.p1) / p.p4)
+		y = (1 + p.p3) / (sqrt(x) + p.p3)
+		dgf = photonenergy / (ΔE+ p.p4) * p.p1 * (x ^ (-3.5 - lb + 0.5 * p.p2)) * y ^ p.p2
 
 		# dgf is in units of hartree^-1 [1 / energy]
 		# function logic to calculate dgf has been verified
@@ -235,27 +170,25 @@ begin
 		end
 	end
 	
-	function photocrosssection(ν, p, gb, gf, lb, lf, thermalenergy)
+	function photocrosssection(E, p::Prr, gb, gf, lb, lf, thermalenergy)
 		# gives the crosssection in cm^2 of the species for an incident photon energy
-		
-		photonenergy = h * ν
 
-		if photonenergy <= thermalenergy
+		if E <= thermalenergy
 			return 0
 		end
 		
-		dgf = bfoscstrength(photonenergy, p, lb, thermalenergy)
+		dgf = bfoscstrength(E, p, lb, thermalenergy)
 
 		σ = (2 * π * α / gb) * dgf * a0 ^2
 		return σ
 	end
 
-	function radiativecrosssection(ν, p, gb, gf, lb, lf, thermalenergy)
+	function radiativecrosssection(E, p::Prr, gb, gf, lb, lf, thermalenergy)
 		# recombination cross section of radiative recombinations
 		# the formula provided in FAC requires units of Hartree
 		δ = 1e-7 # factor for numerical stability because of the 1/ε term
 		
-		ω = h * ν * eV_to_Hartree
+		ω = E * eV_to_Hartree
 		#println("ω: $ω")
 		
 		ε = ω - thermalenergy * eV_to_Hartree
@@ -276,21 +209,87 @@ begin
 		coeff = α ^ 2 / 2 * (gb / gf) * ω ^ 2 / denom
 		#println("c: $coeff")
 
-		σ = photocrosssection(ν, p, gb, gf, lb, lf, thermalenergy)
+		σ = photocrosssection(E, p, gb, gf, lb, lf, thermalenergy)
 		# photocrosssection already returns the values in cm^2
 		#println("σ: $σ")
-		
-		return coeff * σ
+
+		xsection = coeff * σ
+		if xsection <= 1e-25
+			return 0
+		else 
+			return xsection
+		end
 	end
 end
+
+# ╔═╡ f1a53e3c-fd60-4c8c-8931-66a92e28dc36
+begin
+	nelectrons = 4
+	OxygenRRfile = joinpath(fac_dir, "O0$(nelectrons)a.rr")
+	RRheader, RRdata = rfac.read_rr(OxygenRRfile)
+
+	nblocks = pyconvert(Int64, RRheader["NBlocks"])
+
+	for i in 0:(nblocks-1)
+		# Iterating across all the blocks
+		# RRdata is still a python object at this point ∴ 0-indexed
+		
+	end
+	
+	# extracting vectors from the blocks
+	energy, params, bindex, b2j, findex, f2j, deltal = rrblockreader(RRdata[1])
+	prrvec = rrparamblockunpacker(params)
+
+	# masks to avoid unnecessary computation
+	bmask = bindex .!= 0  # only to excited states
+	fmask = findex .== 46 # only from ground state of O03
+	
+	# total mask of case B recombination
+	tmask = bmask .&& fmask
+
+	# filtering our values to the ones we really care about
+	filtered_energy = energy[tmask]
+	filtered_prrvec = prrvec[tmask]
+	filtered_bindex = bindex[tmask]
+	filtered_b2j    = b2j[tmask]
+	filtered_findex = findex[tmask]
+	filtered_f2j    = f2j[tmask]
+	filtered_deltal = deltal[tmask]
+
+	num = length(filtered_energy)
+	testenergies = collect(50:.1:125)
+	crosssections = zeros(length(testenergies))
+
+	for j in 1:1:num
+	# will need to use lookup table to get free lb, lf does not matter.
+	# radiativecrosssection(E, p::Prr, gb, gf, lb, lf, thermalenergy)
+		for (i, energy) in enumerate(testenergies)
+			σ = radiativecrosssection(energy, 
+						   filtered_prrvec[j],
+						   filtered_b2j[j] + 1,
+						   filtered_f2j[j] + 1,
+						   1,
+						   0,
+						   filtered_energy[j]
+						  )
+			crosssections[i] += σ
+		end
+		println(j)
+	end
+end
+
+# ╔═╡ 51e01d0a-a289-4ba0-87b0-f0ba3d5e6973
+plot(testenergies, crosssections ./ 1e-20)
+
+# ╔═╡ 451690bc-74ba-461b-bb1d-f63011d8db08
+rrparamblockunpacker(params)
 
 # ╔═╡ Cell order:
 # ╠═fde54e19-d6fe-4134-84fb-c39be3c0eea2
 # ╠═06a00479-13de-4fc2-83c7-ba263895ad86
 # ╠═8905abb5-93df-4271-8ac2-081b2db56665
-# ╠═d79c83b9-f3e0-497a-8ddc-1473f789ac99
-# ╠═8100381f-fe1a-46fd-a039-cd4d77c29d55
 # ╠═f1a53e3c-fd60-4c8c-8931-66a92e28dc36
+# ╠═51e01d0a-a289-4ba0-87b0-f0ba3d5e6973
 # ╠═fbb8819e-da92-4cd7-a3f0-811a27aa3ae4
 # ╠═1256163d-1be6-4742-a080-baf5136caf83
 # ╠═451690bc-74ba-461b-bb1d-f63011d8db08
